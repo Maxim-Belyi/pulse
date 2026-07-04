c22be8b9bc9663a020f9721bc5a95f3f354bbdef
Maxim-Belyi <btf9458@gmail.com>
2026-07-04 20:28:42 +0500

    feat(): logger

---
+++ pkg/logger/logger.go
@@ -0,0 +1,46 @@
+package logger
+
+import (
+	"io"
+	"log"
+	"os"
+
+	"github.com/rs/zerolog"
+)
+
+type Logger struct {
+	log *zerolog.Logger
+}
+
+func NewLogger(level string, isDev bool, serviceName string) *Logger {
+
+	zlevel, err := zerolog.ParseLevel(level)
+	if err != nil {
+		log.Printf("Ошибка уровня: %v", err)
+	}
+
+	var output io.Writer = os.Stdout
+
+	if isDev {
+		output = zerolog.ConsoleWriter{ Out: os.Stdout }
+	}
+
+	internalLog := zerolog.New(output).
+		Level(zlevel).
+		With().
+		Str("service: ", serviceName).
+		Timestamp().
+		Logger()
+
+	return &Logger{
+		log: &internalLog,
+	}
+}
+
+func (l *Logger) Info(msg string) {
+	l.log.Info().Msg(msg)
+}
+
+func (l *Logger) Error(err error, msg string) {
+	l.log.Error().Msg(msg)
+}
