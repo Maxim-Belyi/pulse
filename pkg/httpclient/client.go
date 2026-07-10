402735999b233084290e0198a3c3d7e496568237
Maxim-Belyi <btf9458@gmail.com>
2026-07-10 09:54:14 +0500

    feat(Client): add HTTP client wrapper with retry logic

--- pkg/httpclient/client.go
+++ pkg/httpclient/client.go
@@ -25,5 +25,31 @@ func NewClient(log *logger.Logger, maxRetries int, baseRetryDelay time.Duration,
 }
 
 func (c *Client) Do(req *http.Request) (*http.Response, error) {
-	return c.client.Do(req)
+	var lastErr error
+	var resp *http.Response
+
+	for i := 0; i <= c.maxRetries; i++ {
+		resp, lastErr = c.client.Do(req)
+
+		if lastErr != nil {
+			c.logger.Error(lastErr, "Сетевая ошибка")
+		} else {
+
+			if resp.StatusCode < 400 {
+				return resp, nil
+			}
+			if (resp.StatusCode >= 400) && (resp.StatusCode < 500) && (resp.StatusCode != 429) {
+				return resp, nil
+			}
+			resp.Body.Close()
+
+		}
+		if i == c.maxRetries {
+			c.logger.Info("Ошибка запроса, превышено количество попыток")
+			break
+		}
+		time.Sleep(c.baseRetryDelay)
+	}
+	return resp, lastErr
+
 }
