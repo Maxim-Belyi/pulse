53325312192c938d8472431ee8cbfd76a7ef4cf5
Maxim-Belyi <btf9458@gmail.com>
2026-07-12 15:40:48 +0500

    feat(usecase): collector method add

---
+++ internal/usecase/collector.go
@@ -0,0 +1,59 @@
+package usecase
+
+import (
+	"context"
+	"pulse/internal/entity"
+	"pulse/pkg/logger"
+	"sync"
+	"time"
+)
+
+type Collector struct {
+	logger  *logger.Logger
+	sources []EventSource
+}
+
+func NewCollector(log *logger.Logger, sources ...EventSource) *Collector {
+	return &Collector{
+		logger:  log,
+		sources: sources,
+	}
+}
+
+func (c *Collector) Start(ctx context.Context, interval time.Duration) <-chan *entity.Event {
+	out := make(chan *entity.Event, 100)
+	var wg sync.WaitGroup
+
+	for _, source := range c.sources {
+		wg.Add(1)
+
+		go func(e EventSource) {
+			defer wg.Done()
+
+			ticker := time.NewTicker(interval)
+			defer ticker.Stop()
+
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case <-ticker.C:
+					events, err := e.Fetch(ctx)
+					if err != nil {
+						c.logger.Info("Ошибка!")
+						continue
+					}
+					for _, event := range events {
+						out <- event 
+					}
+				}
+			}
+		}(source)
+
+	}
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
