0154196d5fb9f8ce53e248515dce0b31c15462b3
Maxim-Belyi <btf9458@gmail.com>
2026-07-24 16:43:20 +0500

    feat(clickhouse): hourly trends function add

--- internal/repo/clickhouse/analytics.go
+++ internal/repo/clickhouse/analytics.go
@@ -30,20 +30,18 @@ func (a *AnalyticsRepository) GetTopSources(ctx context.Context, since time.Time
 
 	rows, err := a.dbConn.Query(ctx, query, since)
 	if err != nil {
-		a.logger.Error(err, "ошибка подключения к бд")
+		a.logger.Error(err, "ошибка подключения к бд GetTopSources")
 		return nil, fmt.Errorf("db conn scan: %w", err)
 	}
 	defer rows.Close()
 	result := make([]usecase.SourceStats, 0)
 
 	for rows.Next() {
-
 		var sourceName string
 		var total uint64
 
 		err := rows.Scan(&sourceName, &total)
 		if err != nil {
-			a.logger.Error(err, "ошибка парсинга")
 			return nil, fmt.Errorf("getTopResources scan: %w", err)
 		}
 		stat := usecase.SourceStats{
@@ -57,6 +55,47 @@ func (a *AnalyticsRepository) GetTopSources(ctx context.Context, since time.Time
 		a.logger.Error(err, "ошибка при итерации строк")
 		return nil, fmt.Errorf("getTopResources iteration: %w", err)
 	}
-	
+
+	return result, nil
+}
+
+func (a *AnalyticsRepository) GetHourlyTrends(ctx context.Context, since time.Time) ([]usecase.TrendStat, error) {
+	query := `SELECT 
+    		  toStartOfHour(occurred_at) AS hour_bucket, 
+    		  count() AS total_events
+			  FROM events
+			  WHERE occurred_at >= ?
+			  GROUP BY hour_bucket
+			  ORDER BY hour_bucket ASC WITH FILL STEP 3600`
+
+	rows, err := a.dbConn.Query(ctx, query, since)
+	if err != nil {
+		a.logger.Error(err, "ошибка подключения к бд")
+		return nil, fmt.Errorf("db conn scan: %w", err)
+	}
+	defer rows.Close()
+	result := make([]usecase.TrendStat, 0)
+
+	for rows.Next() {
+		var hourBacket time.Time
+		var totalEvents uint64
+
+		err := rows.Scan(&hourBacket, totalEvents)
+		if err != nil {
+			return nil, fmt.Errorf("db conn scan: %w", err)
+
+		}
+
+		stat := usecase.TrendStat{
+			HourBacket: hourBacket,
+			TotalEvents: totalEvents,
+		}
+		result = append(result, stat)
+	}
+
+	if err = rows.Err(); err != nil {
+		a.logger.Error(err, "ошибка при итерации строк")
+		return nil, fmt.Errorf("GetHourlyTrends iteration: %w", err)
+	}
 	return result, nil
 }
