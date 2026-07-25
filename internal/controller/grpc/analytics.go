a29b9a76b49c12e3a91686328dc7808113f58279
Maxim-Belyi <btf9458@gmail.com>
2026-07-25 11:55:16 +0500

    feat(analytics): add grpc controller for GetTopSources endpoint

--- internal/controller/grpc/analytics.go
+++ internal/controller/grpc/analytics.go
@@ -2,20 +2,40 @@ package grpc
 
 import (
 	"context"
+	pb "pulse/api/pb/analytics_v1"
 	"pulse/internal/usecase"
 	"pulse/pkg/logger"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type AnalyticsController struct {
-	logger *logger.Logger
+	logger  *logger.Logger
 	useCase *usecase.AnalyticsUseCase
 }
 
 func NewAnalyticsController(logger *logger.Logger, useCase *usecase.AnalyticsUseCase) *AnalyticsController {
-	return &AnalyticsController {
-		logger: logger,
+	return &AnalyticsController{
+		logger:  logger,
 		useCase: useCase,
 	}
 }
 
-func (a *AnalyticsController) GetTopSources(ctx context.Context, req *pb.Get)
+func (a *AnalyticsController) GetTopSources(ctx context.Context, req *pb.GetTopSourcesRequest) (*pb.SourceResponse, error) {
+	ParsedTime := req.Since.AsTime()
+	stats, err := a.useCase.GetTopSources(ctx, ParsedTime)
+	if err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+	var result []*pb.SourceStats
+
+	for _, stat := range stats {
+		pbStat := &pb.SourceStats{
+			Source:      stat.Source,
+			TotalEvents: stat.TotalEvents,
+		}
+		result = append(result, pbStat)
+	}
+	return &pb.SourceResponse{Stats: result}, nil
+}
